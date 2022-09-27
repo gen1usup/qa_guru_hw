af3e817d87796aa6f20d176fcbe0ed3eb6d423b5
DLebedev <dmitriy.lebedev@rtits.ru>
2022-09-27 19:32:57 +0300

    add second test

--- automation_practice_form_test.py
+++ automation_practice_form_test.py
@@ -1,21 +1,9 @@
-import time
-
 from selene.support.shared import browser
-from selene import be, have, command
+from selene import be, command
 
 
-# def test_1(open_browser):
-#
-#     browser.open('https://google.com')
-#     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-#     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
-
-def test_first(open_browser):
-    browser.element('[id="submit"]').click()
-    browser.element(".modal-content").should(be.not_.visible)
-
-def test_second(open_browser):
-    # browser.config.timeout()
+def test_form(open_browser):
+    browser.open('/automation-practice-form')
     browser.element('footer').perform(command.js.set_style_visibility_to_hidden)
     browser.element('[id="firstName"]').type('selene')
     browser.element('[id="lastName"]').type('selene1')
@@ -26,10 +14,8 @@ def test_second(open_browser):
     browser.element('//*[@value=1994]').click()
     browser.element('.react-datepicker__month-select').click()
     browser.element('//*[text()="September"]').click()
-    # browser.element("//*[contains(@aria-label,'September')]").element('.react-datepicker__day--030').click()
     browser.element("//*[contains(@class, 'react-datepicker__day--030') and contains(@aria-label,'September')]").click()
     browser.element('[id="submit"]').click()
-    time.sleep(30)
     browser.element(".modal-content").should(be.visible)
 
     # document.querySelector("footer").style.visibility = 'hidden'
--- conftest.py
+++ conftest.py
@@ -1,37 +1,17 @@
 import os
 import pytest
 from selene.support.shared import browser
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 os.environ['GH_TOKEN'] = "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 "
 
 @pytest.fixture()
 def open_browser():
-    caps = webdriver.DesiredCapabilities.CHROME.copy()
-    caps['acceptInsecureCerts'] = True
-    caps['acceptSslCerts'] = True
-    # driver = webdriver.Chrome(desired_capabilities=caps)
-    options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
-    options.add_argument('--ignore-certificate-errors-spki-list')
-    browser.config.driver = webdriver.Chrome(options=options, desired_capabilities=caps)
-
-    # browser.config.browser_name = 'firefox'
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.open('https://demoqa.com/automation-practice-form')
     yield
     browser.close_current_tab()
 
 
 
 
-
-    # options = webdriver.ChromeOptions()
-    # options.add_argument('--ignore-certificate-errors')
-    # options.add_argument('--ignore-ssl-errors')
-    # options.add_argument('--ignore-certificate-errors-spki-list')
-    # browser.config.driver = webdriver.Chrome("E:\drivers\chromedriver_win32\chromedriver.exe", chrome_options=options)
