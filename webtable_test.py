95402692be9a59e7718466c476817d676129ba4b
DLebedev <dmitriy.lebedev@rtits.ru>
2022-10-02 00:54:34 +0300

    done extra ex

--- webtable_test.py
+++ webtable_test.py
@@ -1,9 +1,50 @@
 import time
 
+from selene import have, be
 from selene.support.shared import browser
-from selene import be, command
+from mimesis import Person
+from mimesis.locales import Locale
+
+# def get_same_name():
+#     return str(int(time.time()))
+
+def set_user_info():
+    person = Person(Locale.RU)
+    user_info = []
+    user_info.append(person.first_name())
+    user_info.append(person.last_name())
+    user_info.append(person.email())
+    user_info.append(person.age())
+    user_info.append(person.weight(1000,10000))
+    user_info.append(person.university())
+    return user_info
 
 
-def table_add_delete_change_test(open_browser):
+def test_table_add(open_browser):
     browser.open('/webtables')
-    print(time.gmtime())
+    browser.element('#addNewRecordButton').click()
+    registration_form_fields = browser.elements('.mr-sm-2')
+    user_info = set_user_info()
+    for i in range(6):
+        registration_form_fields[i].type(user_info[i])
+    browser.element('#submit').click()
+    new_element = browser.elements("//*[@class='rt-tbody']/div[4]/div/div")
+    for i in range(2):
+        new_element[i].should(have.text(user_info[i]))
+
+def test_table_delete(open_browser):
+    browser.open('/webtables')
+    browser.element('#delete-record-3').click()
+
+def test_change(open_browser):
+    browser.open('/webtables')
+    user_info = set_user_info()
+    browser.element('#edit-record-2').click()
+    registration_form_fields = browser.elements('.mr-sm-2')
+    for i in range(6):
+        registration_form_fields[i].clear().type(user_info[i])
+    browser.element('#submit').click()
+    new_element = browser.elements("//*[@class='rt-tbody']/div[2]/div/div")
+    for i in range(2):
+        new_element[i].should(have.text(user_info[i]))
+
