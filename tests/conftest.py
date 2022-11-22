4f31093a806efd841ae6651465bf8067d07c635f
gen1usup <715702@gmail.com>
2022-11-22 14:28:00 +0300

    добавлено видео в тесты

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,7 +32,7 @@ def selenoid_without_video():
         "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": False
+            "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
@@ -47,7 +47,7 @@ def selenoid_without_video():
     attach.add_html(browser)
     attach.add_screenshot(browser)
     attach.add_logs(browser)
-    # attach.add_video(browser)
+    attach.add_video(browser)
     browser.quit()
 
 
